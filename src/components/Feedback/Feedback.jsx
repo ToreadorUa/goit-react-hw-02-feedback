80018c55a24520f0c85be1275a8d6e63caded5ef
ToreadorUa <99kava@gmail.com>
2023-07-27 14:09:36 +0200

    1

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,10 @@
+export const Feedback = props => (
+  <div>
+    <h1>Please leave feedback</h1>
+    <div class="buttons">
+      <button type="button">Good</button>
+      <button type="button">Neutral</button>
+      <button type="button">Bad</button>
+    </div>
+  </div>
+);
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,12 @@
+export const Statistics = () => (
+  <div>
+    <h2>Statistics</h2>
+    <ul>
+      <li>Good:</li>
+      <li>Neutral:</li>
+      <li>Bad:</li>
+      <li>Total:</li>
+      <li>Positive feedback:</li>
+    </ul>
+  </div>
+);
